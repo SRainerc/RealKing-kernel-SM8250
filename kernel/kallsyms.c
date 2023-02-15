7d3a3a24337e598b603ee3e066df225ae042273e
SRainerc <hylpcyj@gmail.com>
2023-02-15 20:13:45 +0800

    Revert "BACKPORT: kallsyms: refactor {,module_}kallsyms_on_each_symbol"
    This reverts commit de9669fb226fbc204ac8de9e5bf7a3010fca7e72.

--- kernel/kallsyms.c
+++ kernel/kallsyms.c
@@ -175,10 +175,6 @@ unsigned long kallsyms_lookup_name(const char *name)
 	return module_kallsyms_lookup_name(name);
 }
 
-/*
- * Iterate over all symbols in vmlinux.  For symbols from modules use
- * module_kallsyms_on_each_symbol instead.
- */
 int kallsyms_on_each_symbol(int (*fn)(void *, const char *, struct module *,
 				      unsigned long),
 			    void *data)
@@ -194,7 +190,7 @@ int kallsyms_on_each_symbol(int (*fn)(void *, const char *, struct module *,
 		if (ret != 0)
 			return ret;
 	}
-	return 0;
+	return module_kallsyms_on_each_symbol(fn, data);
 }
 
 static unsigned long get_symbol_pos(unsigned long addr,
